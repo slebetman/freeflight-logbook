2c9912275f37cc2cc50b22bb44a3a754eefe4221
adly <slebetman@gmail.com>
2017-08-11 16:52:32 +0800

    Make log format selectable

--- src/js/routes/settings.js
+++ src/js/routes/settings.js
@@ -1,3 +1,17 @@
+var $ = require('jquery');
+
+var formats = [
+	'Simple',
+	'Glider',
+	'FAI Competition'
+];
+
 module.exports = function (route, state) {
+	if (typeof state.selected_log_format == 'undefined') {
+		state.selected_log_format = 0;
+	}
+
 	state.previousPage = route.pathname;
+	
+	$('.log-format').html(formats[state.selected_log_format]);
 }
