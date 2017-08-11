d8651fb9d6ca082f0d83fe77e62319f9e1bbb639
adly <slebetman@gmail.com>
2017-08-11 16:33:55 +0800

    Fix font and icon size issues

--- src/js/routes/log_formats.js
+++ src/js/routes/log_formats.js
@@ -1,7 +1,23 @@
 var $ = require('jquery');
 
+function checkSelection (format_id) {
+	$('.selection span.icon').removeClass('icon-check');
+	$('.selection[data-format="' + format_id + '"] span.icon').addClass('icon-check');
+}
+
 module.exports = function (route, state) {
+	if (typeof state.selected_log_format == 'undefined') {
+		state.selected_log_format = 0;
+	}
+	
+	checkSelection(state.selected_log_format);
+	
 	$('#back').click(function(){
 		state.push({url: state.previousPage, transition: 'slide-out'});
+	});
+	
+	$('.selection').click(function(){
+		state.selected_log_format = parseInt($(this).data('format'),10);
+		checkSelection(state.selected_log_format);
 	});
 }
