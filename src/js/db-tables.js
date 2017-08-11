022d30dd76a1e1f01ca62ab49a84765a1be99561
adly <slebetman@gmail.com>
2017-08-11 15:53:32 +0800

    Use simplified table layout

--- src/js/db-tables.js
+++ src/js/db-tables.js
@@ -10,41 +10,50 @@ var brick = require('brick');
 
 
 module.exports = {
-	init: function (db) {
+	init: function (db,callback) {
 		db.transaction(function(ctx){
 			ctx.executeSql(`CREATE TABLE IF NOT EXISTS model (
 				id INTEGER PRIMARY KEY AUTOINCREMENT, 
 				name TEXT,
 				notes TEXT,
-				log_format INTEGER
+				picture TEXT,
+				meta TEXT,
 			)`);
 			ctx.executeSql(`CREATE TABLE IF NOT EXISTS log_format (
-				id INTEGER PRIMARY KEY AUTOINCREMENT, 
-				name TEXT
-			)`);
-			ctx.executeSql(`CREATE TABLE IF NOT EXISTS log_fields (
-				log_format INTEGER,
-				field_type INTEGER
-			)`);
-			ctx.executeSql(`DROP TABLE IF EXISTS field_type`);
-			ctx.executeSql(`CREATE TABLE IF NOT EXISTS field_type (
-				id INTEGER,
 				name TEXT,
-				type TEXT
+				meta TEXT
 			)`);
-			ctx.executeSql(`INSERT INTO field_type (id, name, type) VALUES (1,"location","string")`);
-			ctx.executeSql(`INSERT INTO field_type (id, name, type) VALUES (2,"rubber_thickness","length")`);
-			ctx.executeSql(`INSERT INTO field_type (id, name, type) VALUES (3,"rubber_length","length")`);
-			ctx.executeSql(`INSERT INTO field_type (id, name, type) VALUES (4,"windings","number")`);
-			ctx.executeSql(`INSERT INTO field_type (id, name, type) VALUES (5,"backoff","number")`);
-			ctx.executeSql(`INSERT INTO field_type (id, name, type) VALUES (6,"torque","force")`);
-			ctx.executeSql(`INSERT INTO field_type (id, name, type) VALUES (7,"","")`);
-			ctx.executeSql(`INSERT INTO field_type (id, name, type) VALUES (8,"duration","time")`);
-			ctx.executeSql(`INSERT INTO field_type (id, name, type) VALUES (9,"","")`);
+			ctx.executeSql(`CREATE TABLE IF NOT EXISTS location (
+				id INTEGER PRIMARY KEY AUTOINCREMENT, 
+				name TEXT,
+				notes TEXT,
+				picture TEXT
+			)`);
+			ctx.executeSql(`CREATE TABLE IF NOT EXISTS log (
+				id INTEGER PRIMARY KEY AUTOINCREMENT, 
+				model INTEGER,
+				timestamp INTEGER,
+				duration INTEGER,
+				distance INTEGER,
+				distance_unit TEXT,
+				location INTEGER,
+				windings INTEGER,
+				backoff INTEGER,
+				torque DOUBLE,
+				torque_unit TEXT,
+				rubber_length DOUBLE,
+				rubber_length_unit TEXT,
+				rubber_width DOUBLE,
+				rubber_width_unit TEXT,
+				notes TEXT
+			)`);
 		},
 		function(err){
 			alert('Error processing SQL: '+err.code);
+			callback(err);
 		},
-		function(){}
+		function(){
+			callback();
+		}
 	}
 }
