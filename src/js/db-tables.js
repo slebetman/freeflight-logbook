b98b79379faef0c04305ab9aece59461ec9abb37
adly <slebetman@gmail.com>
2017-08-14 10:36:39 +0200

    Initial implementation of db

--- src/js/db-tables.js
+++ src/js/db-tables.js
@@ -8,12 +8,66 @@ var brick = require('brick');
  * But how to support custom fields?
  */
 
+function q (ctx, callback, query) {
+	ctx.executeSql(query, [], function(ctx,result){
+		callback(result.rows);
+	});
+}
+
+function i (ctx, callback, query) {
+	ctx.executeSql(query,[], function (ctx, result) {
+		callback(result.insertId);
+	});
+}
+
+var formats = [
+	{
+		name: 'Simple',
+		meta: {
+			fields: [
+				'duration',
+				'location',
+				'windings',
+				'rubber_length',
+				'rubber_width',
+				'notes'
+			]
+		}
+	},
+	{
+		name: 'Glider',
+		meta: {
+			fields: [
+				'duration',
+				'location',
+				'notes'
+			]
+		}
+	},
+	{
+		name: 'Torque',
+		meta: {
+			fields: [
+				'duration',
+				'location',
+				'torque',
+				'rubber_length',
+				'rubber_width',
+				'notes'
+			]
+		}
+	}
+]
+
+var DB = null;
 
 module.exports = {
-	init: function (db,callback) {
-		db.transaction(function(ctx){
+	setDB: function (db) {
+		DB = db;
+	},
+	init: function (callback) {
+		DB.transaction(function(ctx){
 			ctx.executeSql(`CREATE TABLE IF NOT EXISTS model (
-				id INTEGER PRIMARY KEY AUTOINCREMENT, 
 				name TEXT,
 				notes TEXT,
 				picture TEXT,
@@ -22,15 +76,21 @@ module.exports = {
 			ctx.executeSql(`CREATE TABLE IF NOT EXISTS log_format (
 				name TEXT,
 				meta TEXT
-			)`);
+			)`,[],function(ctx, result){
+				ctx.executeSql('SELECT count(rowid) as count from log_format',[],
+					function(ctx, result) {
+						if (result[0].count <= 0) {
+							this.initLogFormat(ctx);
+						}
+					}
+				);
+			});
 			ctx.executeSql(`CREATE TABLE IF NOT EXISTS location (
-				id INTEGER PRIMARY KEY AUTOINCREMENT, 
 				name TEXT,
 				notes TEXT,
 				picture TEXT
 			)`);
 			ctx.executeSql(`CREATE TABLE IF NOT EXISTS log (
-				id INTEGER PRIMARY KEY AUTOINCREMENT, 
 				model INTEGER,
 				timestamp INTEGER,
 				duration INTEGER,
@@ -47,6 +107,7 @@ module.exports = {
 				rubber_width_unit TEXT,
 				notes TEXT
 			)`);
+			
 		},
 		function(err){
 			alert('Error processing SQL: '+err.code);
@@ -54,6 +115,99 @@ module.exports = {
 		},
 		function(){
 			callback();
-		}
+		});
+	},
+	initLogFormat: function (ctx) {
+		formats.forEach(function(f){
+			ctx.executeSql(
+				brick('INSERT INTO log_format VALUES (?,?)',
+					f.name,
+					JSON.stringify(f.meta)
+				)
+			);
+		});
+	},
+	models: function (callback) {
+		DB.transaction(function(ctx){
+			q(ctx,callback,
+				'SELECT rowid, * FORM model'
+			);
+		});
+	},
+	addModel: function (data, callback) {
+		DB.transaction(function(ctx){
+			i(ctx, callback,
+				brick('INSERT INTO model VALUES (?,?,?,?)',
+					data.name,
+					data.notes,
+					data.picture,
+					data.meta
+				)
+			);
+		});
+	},
+	logs: function (model_id, callback) {
+		DB.transaction(function(ctx){
+			q(ctx, callback,brick(`
+				SELECT
+					log.rowid,
+					log.*, 
+					location.name as location, 
+					location.id as location_id
+				FORM log
+				JOIN location on location.id = log.location
+					where log.model = ?
+				`,
+				model_id
+			));
+		});
+	},
+	addLog: function (data, callback) {
+		DB.transaction(function(ctx){
+			i(ctx, callback,
+				brick('INSERT INTO log VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+					data.model,
+					data.timestamp,
+					data.duration,
+					data.distance,
+					data.distance_unit,
+					data.location,
+					data.windings,
+					data.backoff,
+					data.torque,
+					data.torque_unit,
+					data.rubber_length,
+					data.rubber_length_unit,
+					data.rubber_width,
+					data.rubber_width_unit,
+					data.notes
+				)
+			);
+		});
+	},
+	locations: function (callback) {
+		DB.transaction(function(ctx){
+			q(ctx,callback,
+				'SELECT rowid, * FROM location'
+			);
+		});
+	},
+	addLocation: function (data, callback) {
+		DB.transaction(function(ctx){
+			i(ctx, callback,
+				brick('INSERT INTO location VALUES (?,?,?)',
+					data.name,
+					data.notes,
+					data.picture
+				)
+			);
+		});
+	},
+	log_formats: function (callback) {
+		DB.transaction(function(ctx){
+			q(ctx,callback,
+				'SELECT rowid, * FROM log_format'
+			);
+		});
 	}
 }
